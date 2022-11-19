64960e5bb7a9500168a79a866ce45fe535a15ab0
Tobias Leinss <caruso33@web.de>
2022-11-19 13:43:21 +0100

    Work on join screen

--- pages/join.tsx
+++ pages/join.tsx
@@ -1,21 +1,39 @@
+import { Contract, ethers } from "ethers"
 import { useRouter } from "next/router"
 import { ChangeEvent, useRef, useState } from "react"
+import { AiFillDatabase } from "react-icons/ai"
 import { BiUpload } from "react-icons/bi"
+import { useAccount, useSigner } from "wagmi"
 import PageLayout from "../components/layouts/PageLayout"
 import Spinner from "../components/Spinner"
+import useIsMounted from "../hooks/useIsMounted"
 
-type LoadingState = "not-loaded" | "loading" | "loaded" | "error"
+enum JoinState {
+  Start = "start",
+  UploadingCsv = "uploading-csv",
+  UploadSuccess = "upload-success",
+  UploadFailure = "upload-failure",
+  EncryptUrl = "encrypt-url",
+  MintToken = "mint-token",
+  MintSuccess = "mint-success",
+  MintFailure = "mint-failure",
+}
 
 export default function Join() {
   const router = useRouter()
+  const isMounted = useIsMounted()
+
+  const { data: signer } = useSigner()
+  const { address, isConnected } = useAccount()
+
   const fileRef = useRef<HTMLInputElement | null>()
 
-  const [loadingState, setLoadingState] = useState<LoadingState>("not-loaded")
+  const [joinState, setJoinState] = useState<JoinState>(JoinState.Start)
 
   async function onChangeInputFile(file: File) {
     if (!file) return
 
-    console.log(file)
+    console.log("File: ", file)
 
     uploadToServer(file)
   }
@@ -25,43 +43,71 @@ export default function Join() {
   }
 
   const uploadToServer = async (file: File) => {
-    setLoadingState("loading")
+    setJoinState(JoinState.UploadingCsv)
 
     try {
       const body = new FormData()
       body.append("file", file!)
       // body.append("field", file.name)
 
-      await fetch("/api/uploadFile", {
-        method: "POST",
-        body,
-      })
-      setLoadingState("loaded")
+      await fetch("/api/uploadFile", { method: "POST", body })
+      setJoinState(JoinState.UploadSuccess)
     } catch (e: any) {
       console.error(`An error occured during uploading the file: ${e.message}`)
-      setLoadingState("error")
+      setJoinState(JoinState.UploadFailure)
     }
+  }
+
+  async function onMintToken() {
+    if (!signer || !address) {
+      console.error(`No signer / address found. Need to sign in.`)
+      return
+    }
+
+    const signature = await signer?.signMessage(address)
+
+    console.dir(signature)
+    // const contract: Contract // new Contract()
+
+    // contract.mint()
+
+    console.log("NOT IMPLEMENTED ON_MINT_TOKEN")
+    console.log(`signature ${signature}, message ${address}`)
+
+    // verification later on server
+
+    console.log(`correct message ${address}`)
+
+    const recoveredAddress = ethers.utils.verifyMessage(address, signature)
+    console.log("recovered address", recoveredAddress)
+
+    const recoveredInvalidAddress = ethers.utils.verifyMessage(
+      `0x${address}`,
+      signature
+    )
+    console.log("recovered invalid address", recoveredInvalidAddress)
   }
 
   function showTitle() {
     let title = null
-    switch (loadingState) {
-      case "not-loaded":
+    switch (joinState) {
+      case JoinState.Start:
         title = "Upload a CSV file"
         break
-      case "loading":
+      case JoinState.UploadingCsv:
         title = "Uploading your files..."
         break
-      case "loaded":
+      case JoinState.UploadSuccess:
         title = "URL encrypted. You can mint your token now"
-
-      case "error":
+        break
+      case JoinState.UploadFailure:
         title = (
           <>
             <p>We&apos;re really sorry.</p>
             <p>An error occured, please refresh the page and try again.</p>
           </>
         )
+        break
 
       default:
         break
@@ -73,8 +119,8 @@ export default function Join() {
   function showContent() {
     let content = null
 
-    switch (loadingState) {
-      case "not-loaded":
+    switch (joinState) {
+      case JoinState.Start:
         content = (
           <div className="mx-auto w-1/2 mt-24 py-24 px-8 py-2 border-2 border-dashed border-gray-500 rounded-xl">
             <button
@@ -106,7 +152,7 @@ export default function Join() {
           </div>
         )
         break
-      case "loading":
+      case JoinState.UploadingCsv:
         content = (
           <div className="mx-auto w-1/2 mt-24 py-24 px-8 py-2 border-2 border-dashed border-gray-500 rounded-xl">
             <div className="mx-auto flex flex-col items-center rounded-xl">
@@ -118,9 +164,24 @@ export default function Join() {
           </div>
         )
         break
-      case "loaded":
+      case JoinState.UploadSuccess:
+        content = (
+          <div className="flex flex-col items-center mx-auto mt-24 py-24 px-8">
+            <div className="text-gray-500 bg-black rounded-full py-4 px-4 text-bold text-5xl ">
+              <AiFillDatabase />
+            </div>
+
+            <button
+              className="bg-violet-600 text-white text-bold text-xl rounded-xl mt-24 px-16 py-2"
+              onClick={() => onMintToken()}
+            >
+              Mint token
+            </button>
+          </div>
+        )
+
         break
-      case "error":
+      case JoinState.UploadFailure:
         break
       default:
         break
@@ -133,11 +194,19 @@ export default function Join() {
     <PageLayout>
       <div className="w-full min-h-screen bg-cover">
         <div className="text-center mt-32">
-          <h1 className="font-bold text-4xl leading-tight">{showTitle()}</h1>
+          {!isMounted ? null : !isConnected ? (
+            <h1 className="font-bold text-4xl leading-tight">Please sign in</h1>
+          ) : (
+            <>
+              <h1 className="font-bold text-4xl leading-tight">
+                {showTitle()}
+              </h1>
 
-          <div className="mt-10">Explanatory text, lorem ipsum...</div>
+              <div className="mt-10">Explanatory text, lorem ipsum...</div>
 
-          {showContent()}
+              {showContent()}
+            </>
+          )}
         </div>
       </div>
     </PageLayout>
