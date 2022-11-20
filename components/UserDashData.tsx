8361cf149e1b5d86a74a13e20f19878182e56386
Jonathan Conn <jonathanconn29@gmail.com>
2022-11-20 02:19:28 -0500

    fixed corrupt ipfs uri, added mapping of addresses to list of tokenids to contract, updated dashboard component to iterate the new mapping

--- components/UserDashData.tsx
+++ components/UserDashData.tsx
@@ -1,29 +1,35 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { ethers } from "ethers"
 
 import { useMMContext } from "../context/MMProvider"
 import { useEthersContext } from "../context/EthersProvider"
 import { abi } from "../abis/currentABI"
 
-let nfts:string[] = [];
 
-export default function UserDashData() {
-    
+
+export default function UserDashData() {    
     const mm = useMMContext().mmContext;
     const provider = useEthersContext().ethersContext as ethers.providers.Web3Provider;
+    const [list, setList] = useState<any[]>([]);
 
     const getNFTs = async () => {
         const signer = provider.getSigner();
-        const contract = new ethers.Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_ETH!, abi, provider.getSigner(mm.account!));
-        const nftList = await contract.balanceOf(mm.account!);
-        console.log(nftList);
-        nfts = nftList;
+        const contract = new ethers.Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_ETH!, abi, signer);
+
+        const bal = await contract.balanceOf(mm.account!);
+
+
+        for (let i = 0; i < bal; i++) {
+            const tokenID = await contract.ownerToTokenIds(mm.account!, i);
+            const token = await contract.tokenURI(tokenID);
+            setList(list => [...list, token]);
+            console.log(`tokenID: ${tokenID}, token: ${token}`);
+        }
+
     }
 
     // updates dashboard if mm or provider changes
     useEffect(() => {
-
-
         // only run when mm and provider are defined
         if (provider != undefined && mm != undefined && mm.status == 'connected') {
             console.log('here')
@@ -31,19 +37,17 @@ export default function UserDashData() {
         }
 
     }, [provider, mm]);
-    
+
     return (
+        <div>
+            <p>here</p>
         <ul>
             {
-                nfts.map((nft) => {
-                    return (
-                        <li key={''}>
-                            {nft}
-                        </li>
-                    )
+                list.map((nft, index) => {
+                    return <li key={index}>{nft}</li>
                 })
-                    
             }
         </ul>
+        </div>
     )
 }
