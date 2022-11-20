bca5124343ca1ced25458ec4e65f7be466daf1ea
Jonathan Conn <jonathanconn29@gmail.com>
2022-11-20 04:20:34 -0500

    added in new ABI files, and updated exports

--- abis/currentABI.tsx
+++ abis/currentABI.tsx
@@ -1,2 +1,4 @@
-const abi = require('./SpendDAO.json').abi;
-export { abi };
+const SBT_ABI = require('./SpendDAO.json').abi;
+const ADMIN_ABI = require('./SpendAdmin.json').abi;
+const DAO_ABI = require('./SpendDAO.json').abi;
+export { SBT_ABI, ADMIN_ABI, DAO_ABI };
