757fcb081739cd82aeda4903591b2d67f12c951d
Jonathan Conn <jonathanconn29@gmail.com>
2022-11-27 15:09:56 -0500

    cleaned up admin dashboard

--- components/AdminDashData.tsx
+++ components/AdminDashData.tsx
@@ -5,7 +5,25 @@ import { useMMContext } from "../context/MMProvider"
 import { useEthersContext } from "../context/EthersProvider"
 import { SBT_ABI } from "../abis/currentABI"
 
-
+type Nft = {
+    status: number
+    totalItems: number
+    items: [
+      {
+        id: string
+        createdAt: string
+        cid: string
+        name: string
+        originalname: string
+        size: string
+        metadata: Record<string, any>
+        type: string
+        pinToIPFS: boolean
+        uri: string
+      }
+    ]
+  }
+  
 
 export default function AdminDashData() {
     const mm = useMMContext().mmContext;
@@ -60,11 +78,51 @@ export default function AdminDashData() {
         return (<div><h1>No SBTs minted</h1></div>)
     }
     else {
-        return (<div><ul>{
-            list.map((nft, index) => {
-                return <li key={index}>{nft}</li>
-            })
-        }</ul></div>
+        // return (<div><ul>{
+        //     list.map((nft, index) => {
+        //         return <li key={index}>{nft}</li>
+        //     })
+        // }</ul></div>
+        // )
+        return (
+            <div className="flex flex-col items-center mx-auto mt-24 px-8 py-3">
+            {list.map((item, index) => {
+              const nft = JSON.parse(item) as Nft
+            return (
+              <div
+                key={index}
+                className="flex items-center justify-between rounded-lg bg-white py-5 px-8 shadow-md"
+              >
+                <div className="flex flex-col items-start">
+                  <p className="text-bold text-gray-500">Token Name</p>
+                  <p className="truncate max-w-xs text-bold text-gray-500">
+                    {nft?.items[0].name.substring(0, 8)}
+                  </p>
+                </div>
+                <div className="ml-8 flex flex-col items-start">
+                  <p className="text-bold text-gray-500">SBT Contract</p>
+                  <p className="truncate max-w-xs text-bold text-gray-500">
+                    {process.env.NEXT_PUBLIC_SBT_ADDR!}
+                  </p>
+                </div>
+                <div className="ml-8 flex flex-col items-start">
+                  <p className="text-bold text-gray-500">Rewards</p>
+                  <p className="text-bold text-gray-500">0.1 FIL</p>
+                </div>
+
+                <button
+                  className="ml-8 bg-violet-600 text-white text-bold text-md rounded-md px-6 py-2"
+                //   disabled={!!isBurning}
+                //   onClick={() => burnToken(nft?.items[0].id)}
+                >
+                  {/* {!!isBurning ? <Spinner /> : "Burn"} */}
+                Unlock
+                </button>
+              </div>
+              
+            )
+          })}
+      </div>
         )
     }
 }
